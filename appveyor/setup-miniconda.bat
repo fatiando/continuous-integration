d8195670a4be93263dfc906ce8992dc3c4b770fe
Leonardo Uieda <leouieda@gmail.com>
2018-07-03 10:57:52 -1000

    Add config files and setup script for Appveyor
    The setup script configures conda and installs the requirements file.

---
+++ appveyor/setup-miniconda.bat
@@ -0,0 +1,21 @@
+REM Configure and update conda, then install the dependencies into a new environment
+REM using the file specified in the CONDA_REQUIREMENTS variable.
+
+REM Don't change the prompt or request user input
+conda config --set always_yes yes --set changeps1 no
+
+REM Add conda-forge to the top of the channel list
+conda config --add channels conda-forge
+
+ECHO Updating conda
+ECHO ===============================================
+conda update --quiet conda
+
+ECHO Creating the 'testing' environment
+ECHO ===============================================
+conda create --quiet --name testing python="%PYTHON%" pip
+activate testing
+
+ECHO Installing requirements from file
+ECHO ===============================================
+IF DEFINED CONDA_REQUIREMENTS (conda install --quiet --file %CONDA_REQUIREMENTS%) ELSE (ECHO No requirements file set)
