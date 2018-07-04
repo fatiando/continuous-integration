18e8c2c5d39be401a41368aedc24e35dd6d460cb
Leonardo Uieda <leouieda@gmail.com>
2018-07-03 14:43:57 -1000

    Fix appveyor requirements install (#3)
    Make sure appveyor actually installs the requirements.
    For some reason, have to use `-c conda-forge` to get conda-forge
    packages instead of defaults.
    Upgrade pip to avoid errors when installing the package.
    Need to use `call` to activate the environment, otherwise the script
    crashes.

--- appveyor/setup-miniconda.bat
+++ appveyor/setup-miniconda.bat
@@ -1,21 +1,40 @@
 REM Configure and update conda, then install the dependencies into a new environment
 REM using the file specified in the CONDA_REQUIREMENTS variable.
 
+REM Enable extensions to use the IF DEFINED command
+REM From http://www.robvanderwoude.com/battech_defined.php
+VERIFY OTHER 2>nul
+SETLOCAL ENABLEEXTENSIONS
+IF ERRORLEVEL 1 ECHO Unable to enable extensions
+
+ECHO.
+ECHO Configuring conda
+ECHO ===============================================
 REM Don't change the prompt or request user input
 conda config --set always_yes yes --set changeps1 no
-
 REM Add conda-forge to the top of the channel list
 conda config --add channels conda-forge
 
+ECHO.
 ECHO Updating conda
 ECHO ===============================================
 conda update --quiet conda
 
+ECHO.
 ECHO Creating the 'testing' environment
 ECHO ===============================================
 conda create --quiet --name testing python="%PYTHON%" pip
-activate testing
+REM Need to use call in batch scripts: https://github.com/conda/conda/issues/794
+call activate testing
 
+ECHO.
+ECHO Updating pip
+ECHO ===============================================
+python -m pip install --upgrade pip
+
+ECHO.
 ECHO Installing requirements from file
 ECHO ===============================================
-IF DEFINED CONDA_REQUIREMENTS (conda install --quiet --file %CONDA_REQUIREMENTS%) ELSE (ECHO No requirements file set)
+IF DEFINED CONDA_REQUIREMENTS (conda install --quiet --channel conda-forge --file %CONDA_REQUIREMENTS%) ELSE (ECHO No requirements file set)
+
+ENDLOCAL
